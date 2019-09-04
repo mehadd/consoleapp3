772b016580a378e31d65de79a39a6c4a62b7187f
Mehad Hassan <mehad.hassan@flairstech.com>
2019-09-04 14:24:56 +0200

    add files

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 4;
+            int z = y / x;
+        }
+    }
+}
